b956928d73296dc23e7715930871daa90e27fb10
Rikuto <orikutobasuke@gmail.com>
2024-09-10 12:01:34 +0900

    firstcommit

---
+++ my-react-app/src/components/template/Header.tsx
@@ -0,0 +1,14 @@
+import '../../styles/components/template/header.css'
+
+function Header() {
+
+    return (
+        <>
+            <header className='head'>
+
+            </header>
+        </>
+    )
+}
+
+export default Header
