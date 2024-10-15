b01a1ca3e1d974f7d10c78eb8cf13e7c77af874c
Rikuto <orikutobasuke@gmail.com>
2024-10-15 10:43:24 +0900

    completion: camera&pictureComponent

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,22 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import CameraComponent from './components/common/Camera'
 import './styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-
-        <p>{count}</p>
-
-        <button
-          onClick={() => setCount(count + 1)}
-        >PLUS</button>
-        <button
-          onClick={() => setCount(count - 1)}
-        >MINUS</button>
-      </div>
+      <CameraComponent />
     </>
   )
 }
